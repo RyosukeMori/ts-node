ebef63cad49f45f7ef6503c2f11057201b665d5d
ryosuke.mori <ryosuke.mori@nickellab.net>
2022-02-18 18:58:01 +0900

    udpate hot reload and typescript

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import { out } from '~/module'
+
 const main = async () => {
-  console.log('hello')
+  console.log(process.env.TEST)
 }
 
 main()
+setInterval(() => {}, 30000)
